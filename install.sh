7b24049975eed9d9e5b4c49c93b3744cad79407a
jsmith <jasmith79@gmail.com>
2018-10-08 14:37:40 -0400

    Fixed typos

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ sudo apt install nettools openssh-server
 sudo apt install -y retroarch
 
 # The universe repo only has a couple cores, so...
-sudo add-apt-repository ppa:libretro/stable
+sudo add-apt-repository ppa:libretro/stable -y
 sudo apt-get update
 sudo apt install -y libretro-*
 
@@ -29,9 +29,9 @@ sudo chown -R "$SUDO_USER" /opt/programs
 
 # And now to copy in the existing config file...
 mkdir -p ~/.config/retroarch/autoconfig/
-cp ./configs/Sony PLAYSTATION(R)3 Controller.cfg ~/.config/retroarch/autoconfig
+cp ./configs/Sony\ PLAYSTATION\(R\)3\ Controller.cfg ~/.config/retroarch/autoconfig
 
 # If this fails, you will need to configure the ps3 controller in retroarch. 
 # After configuring the user 1 inputs, there's a menu option called something 
 # like 'save autoconfig' which will save all the inputs to a config file for further use.
-
+sudo chown -R "$SUDO_USER" ~
